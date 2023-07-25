a09e472c3f3850e4d8e2ec22b43709851c0c9d3d
fineput <139127154+fineput@users.noreply.github.com>
2023-07-25 17:19:56 +0300

    Add files via upload

---
+++ src/components/HomepageFeatures/AreaEllipse/AreaEllipse.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import '@site/src/components/HomepageFeatures/TrapezoidPerimeterCalculator/app.css';
+
+function NavBar() {
+  const [activeTab, setActiveTab] = useState('calculator');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="nav">
+        <div
+          className={`nav-item ${activeTab === 'calculator' ? 'active' : ''}`}
+          onClick={() => handleTabChange('calculator')}
+        >
+          Калькулятор
+        </div>
+        <div
+          className={`nav-item ${activeTab === 'instructions' ? 'active' : ''}`}
+          onClick={() => handleTabChange('instructions')}
+        >
+          Інструкція
+        </div>
+        <div
+          className={`nav-item ${activeTab === 'theory' ? 'active' : ''}`}
+          onClick={() => handleTabChange('theory')}
+        >
+          Теорія
+        </div>
+      </div>
+      <div className="content">
+        {activeTab === 'calculator' && <Calculator />}
+        {activeTab === 'instructions' && <Instructions />}
+        {activeTab === 'theory' && <Theory />}
+      </div>
+    </div>
+  );
+}
+
+function Calculator() {
+    const [a, setA] = useState('');
+    const [b, setB] = useState('');
+    const [area, setArea] = useState(0);
+
+    const calculateArea = () => {
+        const sideA = parseFloat(a);
+        const sideB = parseFloat(b);
+
+        if(isNaN(sideA) || isNaN(sideB)){
+            setArea('Invalid');
+        } else{
+            const areaValue = Math.PI * sideA * sideB;
+            setArea(areaValue.toFixed(2));
+        }
+    };
+
+  return <div className="wrapper">
+        <div className="header_container">
+
+            <h1>Онлайн калькулятор. Площа еліпса</h1>
+            
+        </div>
+
+        <div className="content" id="Calculator">
+
+            <div className="content_calculator">
+
+                <div className="content_data_container">
+
+                    <div className="content_data">
+                    
+                        <div className="content_flex">
+
+                            
+                            <div className="a" id="a">
+                                <label for="a">a: </label>
+                                <input type="number" value={a} onChange={(e) => setA(Number(e.target.value))} placeholder='Довжина великої піввісі'/>
+                            </div>
+
+                            <div className="b" id="b">
+                                <label for="b">b: </label>
+                                <input type="number" value={b} onChange={(e) => setB(Number(e.target.value))} placeholder='Довжина малої піввісі'/>
+                            </div>
+                            
+                            <button onClick={calculateArea} className="calculator_button">Знайти площу</button>
+                            
+                        </div>
+                        <div className="image">
+                        </div>                     
+                    </div>
+                </div>
+                    <div className="results">
+                        <h2>Результат:</h2>
+                        <p id="result">S = {area} см<sup>2</sup></p>
+                    </div>
+            </div>
+        </div>
+    </div>;
+}
+
+function Instructions() {
+  // Код компонента інструкцій
+  return <div className="content" id="Instuctions">
+            <div className="content_data_container">
+                <h2>Ввід даних в калькулятор для обчислення площі еліпса</h2>
+                <div className="instuctions_data">
+                    <h3>Ввод чисел:</h3>
+                    <ul>
+                        <li>
+                            Цілі числа.
+                            Наприклад: 2; 0; -6.<br/><br/>
+                        </li>
+                        <li>
+                            Десяткові дроби вводятся через крапку.              
+                            Наприклад: 1.2; -0.4.
+                        </li>
+                    </ul>
+                    <p>Дужки, літери, операції додавання, віднімання, множення, піднесення до степеня та інші в калькуляторах та вправах на даний момент не підтримуються!</p>
+                </div>
+            </div>
+        </div>;
+}
+
+function Theory() {
+  // Код компонента теорії
+  return <div>
+    <div className="content" id="Theory">
+            <h2 className="theory_caption">Теорія. Площа еліпса</h2>
+            <div className="content_theory_container">
+
+                <div className="content_theory">
+                    <p className="theory_descr"><b>Трапеція</b> - геометричне місце точок, сума відстаней, від яких до двох заданих точок F<sub>1</sub> і F<sub>2</sub>, які називаються фокусами еліпса, є стала величина, більша ніж відстань між цими точками F<sub>1</sub>F<sub>2</sub>. Коло є частковим випадком еліпса.</p>
+
+                </div>
+               
+                <div className="theory_formula">
+                    <h3 className="formula_caption"> Формула для обчислення площі еліпса:</h3>
+                    <p className="formula"><b>S = π · a · b</b></p>
+                    <p className="formula_vol">
+                        де S - площа еліпса,<br />
+                        a - довжина великої піввісі еліпса,<br />
+                        b - довжина малої піввісі еліпса,<br />
+                        π = 3.141592.
+                    </p>
+                </div>
+                
+            </div>
+
+        </div>
+  </div>;
+}
+
+export default NavBar;
